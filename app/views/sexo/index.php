94b7a3ba9678396a45e43380b1ff44abbf64e1a6
MarolyAnapa20 <105999521+MarolyAnapa20@users.noreply.github.com>
2025-04-21 21:44:08 -0500

    Actualice index.php
    Le agregue sexo al id

--- app/views/sexo/index.php
+++ app/views/sexo/index.php
@@ -24,7 +24,7 @@
             <?php if (!empty($sexos) && is_array($sexos)): ?>
                 <?php foreach ($sexos as $sexo): ?>
                     <tr>
-                        <td><?php echo htmlspecialchars($sexo['id']); ?></td>
+                        <td><?php echo htmlspecialchars($sexo['idsexo']); ?></td>
                         <td><?php echo htmlspecialchars($sexo['nombre']); ?></td>
                         <td>
     <a href="/google6b/public/sexo/edit?id=<?php echo htmlspecialchars($sexo['id']); ?>">
